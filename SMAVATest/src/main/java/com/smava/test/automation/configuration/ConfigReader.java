e07588e11d971d3bfec48b8bbd3a2894fb2bf45c
akhilapoosapati <39486091+akhilapoosapati@users.noreply.github.com>
2018-08-10 12:37:13 +0530

    Add files via upload

---
+++ SMAVATest/src/main/java/com/smava/test/automation/configuration/ConfigReader.java
@@ -0,0 +1,14 @@
+package com.smava.test.automation.configuration;
+
+
+
+public interface ConfigReader {
+	
+	public String getEmailId();
+	public String getPassword();
+	public String getUrl();
+	public int getPageLoadTimeOut();
+	public String getBrowser();
+	public String getOS();
+
+}
